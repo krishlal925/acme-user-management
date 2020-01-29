59d775a345dfc7cdd546848928b17f48eb52b5d0
Neel <krishlal925@gmail.com>
2020-01-28 20:34:54 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import qs from 'qs';
+import axios from 'axios';
+
+const API = 'https://acme-users-api-rev.herokuapp.com/api';
+
+const Nav = ({ view, setView, users })=> {
+  return (
+    <nav>
+      <a href={`#${qs.stringify({view: 'home'})}`} className={ view === 'home' ? 'selected': ''} >
+        Home
+      </a>
+      <a href={`#${qs.stringify({view: 'users'})}`} className={ view === 'users' ? 'selected': ''} >
+        Users ({ users.length })
+      </a>
+    </nav>
+  );
+}
+
+const Users = ({users, params}) => {
+
+  console.log({users})
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {
+          users.map( user => {
+            return (
+              <li key={ user.id }>
+                {/* <a href={`#${qs.stringify({view: "users", id: user.id})}`}> { user.fullName } </a> */}
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+//{`#${qs.stringify({view: "user", id: user.id})}`}
+
+const getHash = ()=> {
+  return window.location.hash.slice(1);
+}
+
+
+
 
 function App() {
+  const [ params, setParams ] = useState(qs.parse(getHash()));
+  const [users, setUsers] = useState([])
+
+  useEffect(()=> {
+    window.addEventListener('hashchange', ()=> {
+      setParams(qs.parse(getHash()));
+    });
+    setParams(qs.parse(getHash()));
+  }, []);
+
+  useEffect(()=> {
+    axios.get(`${API}/users`)
+      .then( response => setUsers( response.data ))
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav
+        users = {users}
+        view = {params}
+        setView = {setParams}
+        />
+      <main>
+        { params.view === 'users' && <Users users={ users } params = {params}/> }
+      </main>
     </div>
   );
 }
